eec329bc306e65d0bf9a29c377f784d5c740f527
SyangSouzaa <syangsouza05@gmail.com>
2026-05-19 19:23:09 -0300

    Corrigindo links

--- js/tmb.js
+++ js/tmb.js
@@ -1,27 +1,32 @@
-function calculatorTMB() {
-    
+function calculateTMB(event) {
+    event.preventDefault()
 
-    const gender = document.getElementById("gender").Value
+    const gender = document.getElementById("gender").value
+
     const age = parseInt(document.getElementById("age").value)
-    const weight = parseFloat(documento.getElementById("weight").value)
-    const height = parseInt(documento.getElementById("height").value)
+
+    const weight = parseFloat(document.getElementById("weight").value)
+
+    const height = parseInt(document.getElementById("height").value)
 
     const resultBox = document.getElementById("result-box")
     const valueText = document.getElementById("tmb-value")
+
 
     if(!age || !weight || !height) {
         alert("Preencha todos os campos corretamente")
         return
     }
 
-    let tbm = 0
+    let tmb = 0
 
-    if (gender === "male") {
+    if (gender === 'male') {
         tmb = 88.36 + 13.4 * weight + 4.8 * height - 5.7 * age
     } else {
         tmb = 447.6 + 9.2 * weight + 3.1 * height - 4.3 * age
     }
 
     valueText.innerText = Math.round(tmb)
-    resultBox.classList.add("show")
+
+    resultBox.classList.add('show')
 }
